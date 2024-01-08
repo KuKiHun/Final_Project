ca0bc79dac3a13e6ad56695323d8dc203e71f88c
minjin1324 <bigpig1324@gmail.com>
2024-01-08 15:16:41 +0900

    minjin commit

---
+++ src/main/java/com/example/dao/BoardDAO.java
@@ -0,0 +1,14 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.domain.BoardVO;
+
+@Mapper
+public interface BoardDAO {
+	
+	public List<BoardVO> selectList();
+	
+}
